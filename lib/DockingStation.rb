d253f140342b407dc6f9365c83dc28a7c484f9c8
Adam Woodcock <adam.woodcock98@gmail.com>
2022-02-22 16:59:00 +0000

    Added additional tests to docking_station_spec  and changed file structure

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,5 +1,13 @@
 class DockingStation
   def release_bike 
+     bike = Bike.new
 
+     return bike
+  end
+end
+
+class Bike
+  def working?
+    true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,9 @@ require "DockingStation.rb"
 describe DockingStation do
   it { expect(subject).to respond_to(:release_bike) }
 end
+
+describe DockingStation do
+  it { expect(subject.release_bike).to be_a Bike }
+
+  it { expect(subject.release_bike.working?).to eq true }
+end
